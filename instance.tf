425647231de5cbffc8feabf39bbb54d81f787861
Bhargav Siripurapu <104213005+bargavsiripurapu@users.noreply.github.com>
2023-04-18 15:35:17 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "example" {
   ami           = var.AMIS
   instance_type = "t2.micro"
-
+  tags = {
+    Name = "Terra-EC2"
+  }
   # the VPC subnet
   subnet_id = aws_subnet.main-public-1.id
 
